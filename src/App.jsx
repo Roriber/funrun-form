e39a8e65193c35ec8fe1cd83a617f58943a3def0
Roland Oliverr <rolandpetrola11@gmail.com>
2026-01-14 19:28:14 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import FunRunForm from "./FunRunForm.jsx";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function App() {
+  return <FunRunForm />;
+}
